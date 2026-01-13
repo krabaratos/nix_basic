f401c0b4bf5def4bfd6a11d43ec9755b20474de2
Krabaratos <63770336+krabaratos@users.noreply.github.com>
2026-01-13 02:17:52 -0800

    Add files via upload

---
+++ configuration.nix
@@ -0,0 +1,55 @@
+{ config, lib, pkgs, ... }:
+
+{
+  imports =
+    [ ./hardware-configuration.nix
+    ];
+  #Flakes
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Use latest kernel.
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  networking.hostName = "nixos"; # Define your hostname.
+
+  # Configure network connections interactively with nmcli or nmtui.
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Asia/Kolkata";
+
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+  services.displayManager.ly.enable = true;
+
+   #USER Settings
+   users.users.manish = {
+     isNormalUser = true;
+     extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+     packages = with pkgs; [
+      tree
+     ];
+   };
+
+  #System Pakcges
+  environment.systemPackages = with pkgs; [
+ hyprland
+ wofi
+ firefox
+ hyprpaper
+ hyprpicker
+ nautilus
+ waybar
+ htop
+ fastfetch
+ ];
+
+ system.stateVersion = "25.11"; # Did you read the comment?
+
+}
+
